b125c3b07218a9532e702a64824daf1f5a8e24ff
Subash S <subash.s2024aids@sece.ac.in>
2025-12-16 09:03:22 +0530

    Add files via upload

---
+++ DAY 12/app/src/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require("../models/userModel");
+
+// GET all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// CREATE user (Signup)
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
